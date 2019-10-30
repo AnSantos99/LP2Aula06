95b896c22b959bd023218fbd6f6651887ba90a75
AnSantos99 <an.santos95@hotmail.com>
2019-10-30 15:13:16 +0000

    Adapter Class

---
+++ BearAdapter/BearAdapter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdapterTest
+{
+    class BearAdapter : IDog
+    {
+        private IBear bear;
+
+        public BearAdapter(IBear bear) 
+        {
+            this.bear = bear;
+        }
+        public void Bark()
+        {
+            bear.Roar();
+        }
+
+        public void Fetch(object objectToFetch)
+        {
+            bear.GoTowards(objectToFetch);
+            bear.LookAt(objectToFetch);
+            if(bear.TryEat(objectToFetch))
+                    Console.WriteLine("Yay i ate the " + objectToFetch);
+
+            else
+                Console.WriteLine("oops i will not eat this");
+        }
+    }
+}
---
+++ BearAdapter/BrownBear.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdapterTest
+{
+    class BrownBear : IBear
+    {
+        Random rnd;
+
+        public BrownBear(bool hibernating = false) 
+        {
+            rnd = new Random();
+        }
+
+
+        public bool Hibernating { get; set; }
+
+        public void GoTowards(object objectToWalkTowards)
+        {
+            if (!Hibernating) 
+                Console.WriteLine("Bear moving towards " + objectToWalkTowards);
+        }
+
+        public void LookAt(object objectToLookAt)
+        {
+            if (!Hibernating)
+                Console.WriteLine("Bear looking at " + objectToLookAt);
+        }
+
+        public void Roar()
+        {
+            if (Hibernating) Console.WriteLine("ZZZzzzz");
+            else Console.WriteLine("Roaaaaaaaar!!");
+        }
+
+        public bool TryEat(object objectToEat)
+        {
+            if (!Hibernating && rnd.NextDouble() < 0.7)
+                return true;
+            return false;
+                
+        }
+    }
+}
